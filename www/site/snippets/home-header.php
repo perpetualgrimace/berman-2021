acae5a9c34170bf3b8372f13eb41502e0fc3ea61
perpetualgrimace <hello@jamesferrell.me>
2021-05-09 11:24:34 -0400

    kills scroll-indicator

--- www/site/snippets/home-header.php
+++ www/site/snippets/home-header.php
@@ -18,16 +18,5 @@
    <? snippet('home-header-hero') ?>
 </header>
 
-<div class="scroll-indicator-container">
-  <a class="scroll-indicator-link button-inverted" href="#intro" tabindex="-1"  aria-hidden="true">
-
-    <svg class="scroll-indicator-svg" width="36px" height="22px" viewBox="-90 -81 36 22">
-      <polygon fill="#2629A8" points="-85.3875005 -81 -72 -67.4021594 -58.6124997 -81 -54.5 -76.8137655 -72 -59 -89.5 -76.8137655"></polygon>
-    </svg>
-
-    <span class="u-screenreader"></span>
-
-  </a>
-</div>
 
 <div class="g-container u-padding-bottom-off u-padding-top-off">
